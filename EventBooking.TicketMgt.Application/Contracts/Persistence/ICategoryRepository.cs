2b6d838a91947ba68868cc7ef6e5220e6316452d
Seyi <suberuseyi@gmail.com>
2026-08-28 22:34:26 +0100

    Contracts defined in the Core Application project

---
+++ EventBooking.TicketMgt.Application/Contracts/Persistence/ICategoryRepository.cs
@@ -0,0 +1,7 @@
+﻿
+namespace EventBooking.TicketMgt.Application.Contracts.Persistence;
+
+public interface ICategoryRepository:IAsyncRepository<Category> 
+{
+
+}
--- EventBooking.TicketMgt.Application/GlobalUsings.cs
+++ EventBooking.TicketMgt.Application/GlobalUsings.cs
@@ -1,1 +1,1 @@
-﻿global using EventBooking.TicketMgt.Domain;
+﻿global using EventBooking.TicketMgt.Domain.Entities;
